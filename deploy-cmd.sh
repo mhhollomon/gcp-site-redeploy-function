3c462f407545ce1370145e092520f5f274b66656
Mark Hollomon <mhhollomon@gmail.com>
2019-12-16 06:10:14 -0500

    fix up the deploy script a bit

--- deploy-cmd.sh
+++ deploy-cmd.sh
@@ -1,14 +1,19 @@
-TOPIC="netlify-deploy"
+#!/usr/bin/bash -x
 
-gcloud functions deploy redeploy --runtime nodejs10 \
-    --trigger-topic ${TOPIC} \
-    --project ${PROJECT_ID} \
-    --set-env-vars EMAIL_API_KEY=${EMAIL_API_KEY}
+TOPIC="netlify-redeploy-trigger"
+
+if [ "$1" = "function" ]; then
+    shift
+    gcloud functions deploy redeploy --runtime nodejs10 \
+        --trigger-topic ${TOPIC} \
+        --project ${PROJECT_ID} \
+        --set-env-vars EMAIL_API_KEY=${EMAIL_API_KEY}
+fi
 
 if [ "$1" = "trigger" ]; then
 	gcloud scheduler jobs create pubsub netlify-redeploy  \
 		--project ${PROJECT_ID} \
-		--timezone="America/New_York" --topic=${TOPIC} --schedule="10 5 * * *" \
+		--time-zone="America/New_York" --topic=${TOPIC} --schedule="10 5 * * *" \
 		--message-body="{}" --description="trigger the redeploy function"
 fi
  
